59ca7ea6194d0f19beb3ddef96befd0e212fc092
Takahiro Kawasaki <63230525+Hiro626@users.noreply.github.com>
2021-04-19 13:49:33 -0400

    put w8 code back at bottom

--- public/script.js
+++ public/script.js
@@ -207,3 +207,23 @@ async function windowActions() {
 }
 
 window.onload = windowActions();
+
+// async function getDiningData() {
+//     const request = await fetch('/api/dining');
+//     const dining = await request.json();
+//     const diningData = dining.data;
+
+//     const diningTable = document.querySelector('.table');
+
+//     diningData.forEach((item) => { 
+//         const appendItem = document.createElement('tr');
+//         appendItem.innerHTML = `
+//             <td>${item.hall_id}</td>
+//             <td>${item.hall_name}</td>
+//             <td>${item.hall_address}</td>
+//         `;
+//         diningTable.append(appendItem);
+//     });
+// }
+
+// window.onload = getDiningData();
